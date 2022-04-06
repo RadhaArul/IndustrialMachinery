d1798c933462395920ff5a24d85b936bc035485f
Radha Manickam <radhaarul@gmail.com>
2022-04-06 09:56:34 +0200

    Add project files.

---
+++ IndustrialMachinery.Api/Repositories/MachineRepository.cs
@@ -0,0 +1,33 @@
+﻿using IndustrialMachinery.Core;
+
+namespace IndustrialMachinery.Api.Repositories
+{
+    public class MachineRepository : IMachineRepository
+    {
+
+        public Task<Machine> AddMachine(Machine machine)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMachine(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Machine>> GetAllMachines()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Machine> GetMachineById(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Machine> UpdateMachine(Machine machine)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IndustrialMachinery.Core/Machine.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IndustrialMachinery.Core
+{
+    public class Machine
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+        public string Data { get; set; }
+
+
+    }
+}
